7467665c588c1b2b662bb47f6821695319fe3943
Ahmad <ahmad.waliullah.khan@gmail.com>
2020-03-08 02:28:32 +0530

    S3 storage fix

--- photos/settings.py
+++ photos/settings.py
@@ -116,8 +116,8 @@ AUTH_PASSWORD_VALIDATORS = [
 AWS_QUERYSTRING_AUTH = False
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['django-advance-photos-app']
-MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % django-advance-photos-app
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET']
+MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
 
 # Internationalization
